606c25bffbc8be7eb94f67b4f5d224c98652e592
Seongha Hong <dkqk01242@gmail.com>
2026-08-07 16:47:14 +0900

    test: KMP 공통 테스트 환경 구성

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.metro)
+    alias(libs.plugins.mokkery)
 }
 
 kotlin {
@@ -64,6 +65,9 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+            implementation(libs.ktor.clientMockKmp)
+            implementation(libs.turbine)
         }
     }
 }
